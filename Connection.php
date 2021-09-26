07677166c25d6a197bdf79800ef413adc2a1459d
0701Vatsal <vatsalshukla0701@gmail.com>
2021-09-26 18:29:07 +0530

    Update

--- Connection.php
+++
@@ -1,9 +0,0 @@
-<?php 
-		
-	$host = "localhost";
-	$user = "root";
-	$password = "";
-	$dbname = "maid_service";
-	$conn=mysqli_connect($host, $user , $password ,  $dbname);
-	//echo "Connection Successfully..!";
-?>
